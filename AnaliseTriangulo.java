1f3e2c8b7213eac6470c50db1cc025c91af1d572
Pehc <Tijolinhodopagode@gmail.com>
2025-08-18 22:03:37 -0300

    AttFinal 18/08/25

---
+++ AnaliseTriangulo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class AnaliseTriangulo {
+    public static void main(String[] args) {
+        System.out.println("Programa de identificação de triangulos");
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            System.out.println("Diga a medida do primeiro lado: ");
+            double lado1 = scanner.nextDouble();
+            System.out.println("Diga a medida do segundo lado: ");
+            double lado2 = scanner.nextDouble();
+            System.out.println("Diga a medida do terceiro lado: ");
+            double lado3 = scanner.nextDouble();
+
+            if (
+                (lado1 <= (lado2 + lado3 ))&&
+                (lado2 <= (lado1 + lado3 ))&&
+                (lado3 <= (lado2 + lado1 ))
+             ){
+                System.out.println("As medidas informadas formam um Triangulo");
+             } else {
+                System.out.println("As medidas informadas não formam um Triangulo");
+             }
+
+        } catch (Exception ex) {
+            System.out.println("Algo deu errado! tente novamente");
+        } finally {
+            scanner.close();
+        }
+    }
+}
