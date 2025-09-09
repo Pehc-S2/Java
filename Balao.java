db115494e71451b213ec29da6e898af5b3093450
Pehc <Tijolinhodopagode@gmail.com>
2025-09-08 21:35:42 -0300

    Att1 - 08/09/25

---
+++ Balao.java
@@ -0,0 +1,21 @@
+//Atributos
+public class Balao {
+    String cor;
+    double tamanho;
+    String nome;
+    int idade;
+    int ordem;
+
+    //Ações
+    Balao(String cor, double tamanho, String nome, int idade, int ordem){
+        this.cor = cor;
+        this.tamanho = tamanho;
+        this.nome = nome;
+        this.idade = idade;
+        this.ordem = ordem;
+    }
+
+    void estourar(){
+        System.out.println("POP!");
+    }
+}
---
+++ Ordenacao.java
@@ -0,0 +1,6 @@
+public class Ordenacao {
+    public static void main(String[] args) {
+        Balao a = new Balao("Azul", 24, "Pabblo", 20, 27);
+        System.out.println(a.cor);
+    }
+}
